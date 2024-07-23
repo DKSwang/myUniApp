c1974007193aa1f5c460ff4703906522e2e79ae5
DKSwang <46430506+DKSwang@users.noreply.github.com>
2024-07-23 16:32:12 +0800

    scroll-view标签的应用

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -10,6 +10,20 @@
 			BB
 			<text selectable space="emsp">我是可以被        拷贝的text</text>
 		</view>
+		
+		<view class="line">scroll-view</view>
+		
+		<scroll-view scroll-y="true" class="scroll_part">
+			<view  v-for="(item,index) in 100">
+				{{item}}
+			</view>
+		</scroll-view>
+		
+		<scroll-view scroll-x="true" class="scroll_part">
+			<view class="scroll_inner" v-for="(item,index) in 100">
+				{{item}}
+			</view>
+		</scroll-view>
 	</view>
 </template>
 
@@ -20,7 +34,6 @@
 
 <style lang="scss"> 
 .layout{
-	border:1px solid red;
 	.partA{
 		width:100px;
 		height:100px;
@@ -40,6 +53,29 @@
 	
 	.partB{
 		color: green;
+	}
+	.line{
+		width: 100%;
+		height:2px;
+		background:green;
+		margin:15px 2px;
+		text-align:center;
+		color:red;
+		font-weight:600;
+	}
+	.scroll_part{
+		margin:0 auto;
+		width:90%;
+		height:300px;
+		border:1px solid #ccc;
+		white-space: nowrap;
+		.scroll_inner{
+			width:100px;
+			height:20px;
+			background:green;
+			display:inline-block;
+			margin:5px;
+		}
 	}
 }
 	
