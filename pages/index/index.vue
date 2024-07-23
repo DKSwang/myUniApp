5e0dc9d4d634312e8eb8513564c8e7164b5368a1
DKSwang <46430506+DKSwang@users.noreply.github.com>
2024-07-23 15:48:29 +0800

    新建页面

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,28 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="layout">
+		<view class="partA">
+			AAA
+		</view>
+		<view class="partB">
+			BBB
 		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+<script setup>
+	const title = "你好呀"
+	
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+<style lang="scss"> 
+.layout{
+	border:1px solid red;
+	.partA{
+		color: red;
 	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	.partB{
+		color: Green;
 	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+}
+	
 </style>
